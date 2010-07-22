5f6d7ce049909bc19f193b82dc358d8d828c4503
Justin Santa Barbara <justin@fathomdb.com>
2010-07-22 11:49:13 -0700

    Nobody wants to take on this twisted cleanup.  It works for now, but could be much nicer if twisted has a nice hook-point for exception mapping

--- nova/objectstore/handler.py
+++ nova/objectstore/handler.py
@@ -666,7 +666,7 @@ newline|'\n'
 indent|'    '
 string|'"""Maps exceptions to 404 / 401 codes.  Won\'t work for exceptions thrown after NOT_DONE_YET is returned."""'
 newline|'\n'
-comment|'# TODO: This needs to be plugged in to the right place in twisted...'
+comment|'# TODO(unassigned) (calling-all-twisted-experts): This needs to be plugged in to the right place in twisted...'
 nl|'\n'
 comment|"#   This doesn't look like it's the right place (consider exceptions in getChild; or after NOT_DONE_YET is returned     "
 nl|'\n'
