63edc035f93fe8fd659a4a9e1e71aafd610561c4
Todd Willey <todd@ansolabs.com>
2010-12-28 17:54:31 -0500

    Defualt services to enabled.

--- nova/db/api.py
+++ nova/db/api.py
@@ -32,7 +32,7 @@ comment|'#    License for the specific language governing permissions and limita
 nl|'\n'
 comment|'#    under the License.'
 nl|'\n'
-string|'"""\nDefines interface for DB access.\n\nThe underlying driver is loaded as a :class:`LazyPluggable`.\n\n**Related Flags**\n\n:db_backend:  string to lookup in the list of LazyPluggable backends.\n              `sqlalchemy` is the only supported backend right now.\n\n:sql_connection:  string specifying the sqlalchemy connection to use, like:\n                  `sqlite:///var/lib/nova/nova.sqlite`.\n\n:enable_new_services:  when adding a new service to the database, is it in the\n                       pool of available hardware (Default: False)\n"""'
+string|'"""\nDefines interface for DB access.\n\nThe underlying driver is loaded as a :class:`LazyPluggable`.\n\n**Related Flags**\n\n:db_backend:  string to lookup in the list of LazyPluggable backends.\n              `sqlalchemy` is the only supported backend right now.\n\n:sql_connection:  string specifying the sqlalchemy connection to use, like:\n                  `sqlite:///var/lib/nova/nova.sqlite`.\n\n:enable_new_services:  when adding a new service to the database, is it in the\n                       pool of available hardware (Default: True)\n"""'
 newline|'\n'
 nl|'\n'
 name|'from'
@@ -77,7 +77,7 @@ name|'DEFINE_boolean'
 op|'('
 string|"'enable_new_services'"
 op|','
-name|'False'
+name|'True'
 op|','
 nl|'\n'
 string|"'Services to be added to the available pool on create'"
