ba9e8fcf4ba2a9193e27c08ce955169619991ddb
Andy Smith <code@term.ie>
2011-01-12 16:57:04 -0800

    add support for database migration

---
+++ nova/db/sqlalchemy/migrate_repo/manage.py
@@ -0,0 +1,33 @@
+begin_unit
+comment|'#!/usr/bin/env python'
+nl|'\n'
+name|'from'
+name|'migrate'
+op|'.'
+name|'versioning'
+op|'.'
+name|'shell'
+name|'import'
+name|'main'
+newline|'\n'
+name|'if'
+name|'__name__'
+op|'=='
+string|"'__main__'"
+op|':'
+newline|'\n'
+indent|'  '
+name|'main'
+op|'('
+name|'debug'
+op|'='
+string|"'False'"
+op|','
+name|'repository'
+op|'='
+string|"'.'"
+op|')'
+newline|'\n'
+dedent|''
+endmarker|''
+end_unit
