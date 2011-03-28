a701de553724f77e8071e32e89664fd522b99d76
Justin Santa Barbara <justin@fathomdb.com>
2011-03-28 16:54:17 -0700

    Multi-line comments should end in a blankline

--- nova/api/openstack/incubator/volumes/volume_attachments.py
+++ nova/api/openstack/incubator/volumes/volume_attachments.py
@@ -247,7 +247,7 @@ op|')'
 op|':'
 newline|'\n'
 indent|'    '
-string|'"""The volume attachment API controller for the Openstack API.\n\n    A child resource of the server.  Note that we use the volume id\n    as the ID of the attachment (though this is not guaranteed externally)"""'
+string|'"""The volume attachment API controller for the Openstack API.\n\n    A child resource of the server.  Note that we use the volume id\n    as the ID of the attachment (though this is not guaranteed externally)\n\n    """'
 newline|'\n'
 nl|'\n'
 DECL|variable|_serialization_metadata
