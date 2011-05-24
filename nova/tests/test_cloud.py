1fe00901492604b6ca5bf11f7317e96a41e1c584
William Wolf <throughnothing@gmail.com>
2011-05-23 22:47:44 -0400

    make image_ref and image_id usage more consistant, eliminate redundancy in compute_api.create() call

--- nova/tests/test_cloud.py
+++ nova/tests/test_cloud.py
@@ -2461,7 +2461,7 @@ newline|'\n'
 name|'kwargs'
 op|'='
 op|'{'
-string|"'image_id'"
+string|"'image_ref'"
 op|':'
 string|"'ami-1'"
 op|','
@@ -2598,7 +2598,7 @@ indent|'        '
 name|'kwargs'
 op|'='
 op|'{'
-string|"'image_id'"
+string|"'image_ref'"
 op|':'
 string|"'ami-1'"
 op|'}'
