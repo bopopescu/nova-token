f2390bb4979338ae5b8865c2a9f1d13940c6930c
Vishvananda Ishaya <vishvananda@gmail.com>
2011-07-21 22:46:57 +0000

    start removing references to AuthManager

--- nova/log.py
+++ nova/log.py
@@ -94,9 +94,9 @@ op|','
 nl|'\n'
 string|"'%(asctime)s %(levelname)s %(name)s '"
 nl|'\n'
-string|"'[%(request_id)s %(user)s '"
+string|"'[%(request_id)s %(user_id)s '"
 nl|'\n'
-string|"'%(project)s] %(message)s'"
+string|"'%(project_id)s] %(message)s'"
 op|','
 nl|'\n'
 string|"'format string to use for log messages with context'"
