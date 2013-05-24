ec00cdf21e82c3b55551df53d7bfd5074ec61da8
Rick Harris <rconradharris@gmail.com>
2013-05-24 23:31:01 +0000

    Moving more tests to appropriate locations
    Change-Id: I06aa8d8f31593cfea6e26560d6af2caf6a29194d

--- nova/tests/db/test_migrations.py
+++ nova/tests/db/test_migrations.py
@@ -10522,7 +10522,7 @@ name|'__file__'
 op|')'
 op|','
 nl|'\n'
-string|"'virt/baremetal/test_baremetal_migrations.conf'"
+string|"'../virt/baremetal/test_baremetal_migrations.conf'"
 op|')'
 newline|'\n'
 comment|'# Test machines can set the NOVA_TEST_MIGRATIONS_CONF variable'
