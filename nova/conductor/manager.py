b4b50854f474bdc94e1e097196b72828c504b79b
Dan Smith <dansmith@redhat.com>
2014-04-11 15:32:57 -0700

    Deprecate instance_get_all_by_filters() from conductor
    This deprecates the instance_get_all_by_filters() method from
    conductor, which is no longer used.
    
    This also removes a test that was mocking the conductor method,
    but that really tests nothing at all. Perhaps a new test to ensure
    that the period=0 case is handled properly is in order, but this
    test definitely wasn't doing that.
    
    Related to blueprint compute-manager-objects-juno
    
    Change-Id: I1e7bc19296d1773bbc2f5f9e77a1faf0e6ac1fe6

--- nova/conductor/manager.py
+++ nova/conductor/manager.py
@@ -6745,6 +6745,8 @@ name|'legacy'
 op|')'
 newline|'\n'
 nl|'\n'
+comment|'# TODO(danms): This can be removed in version 3.0 of the RPC API'
+nl|'\n'
 DECL|member|instance_get_all_by_filters
 dedent|''
 name|'def'
