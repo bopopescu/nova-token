02c3136f670b32e99effe90b531265968aa08a88
Silvan Kaiser <silvan@quobyte.com>
2014-12-18 17:43:33 +0100

    Libvirt: Created Nova driver for Quobyte
    Driver for Quobyte storage product.
    Mounts respective Quobyte volume of VM images.
    This is the Nova part of integrating Quobyte.
    Corresponding Cinder Driver Blueprint:
    https://blueprints.launchpad.net/cinder/+spec/quobyte-usp-driver
    The Cinder driver has been merged into Cinder,
    it's review can be found at:
    https://review.openstack.org/#/c/94186/
    
    blueprint quobyte-nova-driver
    DocImpact: Adding option 'quobyte_mount_point_base' and 'quobyte_client_cfg'
    
    Change-Id: Ica1820031f1fc8b66d7ed7fe76ffeb985cf0ef35

--- nova/virt/libvirt/driver.py
+++ nova/virt/libvirt/driver.py
@@ -932,6 +932,11 @@ nl|'\n'
 string|"'nova.virt.libvirt.volume.LibvirtGPFSVolumeDriver'"
 op|','
 nl|'\n'
+string|"'quobyte='"
+nl|'\n'
+string|"'nova.virt.libvirt.volume.LibvirtQuobyteVolumeDriver'"
+op|','
+nl|'\n'
 op|']'
 op|','
 nl|'\n'
