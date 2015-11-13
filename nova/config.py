8a7ea6a4ea6f7f14fd61f12518b273f6a3f66a22
Ronald Bradford <ronald.bradford@gmail.com>
2015-11-13 16:27:07 -0500

    Remove duplicated oslo.log configuration setup
    Simplified the definition of Nova specific log levels by extending the
    oslo.log standard levels used of all projects. This olso.log default
    includes all existing values including suds=INFO.
    
    * Removed logging context format as this matches oslo.log default.
    * Reordered log initiation calls to match documented order.
    
    See http://docs.openstack.org/developer/oslo.log/usage.html
    
    Change-Id: Ibb4bf8b50419a578db49953e2fc69f45c5e3edb3

--- nova/config.py
+++ nova/config.py
@@ -115,65 +115,11 @@ string|"'nova.sqlite'"
 op|')'
 newline|'\n'
 nl|'\n'
-comment|'# NOTE(mikal): suds is used by the vmware driver, removing this will'
-nl|'\n'
-comment|'# cause many extraneous log lines for their tempest runs. Refer to'
-nl|'\n'
-comment|'# https://review.openstack.org/#/c/219225/ for details.'
-nl|'\n'
-name|'_DEFAULT_LOG_LEVELS'
+name|'_EXTRA_DEFAULT_LOG_LEVELS'
 op|'='
 op|'['
-string|"'amqp=WARN'"
-op|','
-string|"'amqplib=WARN'"
-op|','
-string|"'boto=WARN'"
-op|','
-nl|'\n'
-string|"'qpid=WARN'"
-op|','
-string|"'sqlalchemy=WARN'"
-op|','
-string|"'suds=INFO'"
-op|','
-nl|'\n'
-string|"'oslo_messaging=INFO'"
-op|','
-string|"'iso8601=WARN'"
-op|','
-nl|'\n'
-string|"'requests.packages.urllib3.connectionpool=WARN'"
-op|','
-nl|'\n'
-string|"'urllib3.connectionpool=WARN'"
-op|','
-string|"'websocket=WARN'"
-op|','
-nl|'\n'
-string|"'keystonemiddleware=WARN'"
-op|','
-string|"'routes.middleware=WARN'"
-op|','
-nl|'\n'
-string|"'stevedore=WARN'"
-op|','
 string|"'glanceclient=WARN'"
 op|']'
-newline|'\n'
-nl|'\n'
-DECL|variable|_DEFAULT_LOGGING_CONTEXT_FORMAT
-name|'_DEFAULT_LOGGING_CONTEXT_FORMAT'
-op|'='
-op|'('
-string|"'%(asctime)s.%(msecs)03d %(process)d '"
-nl|'\n'
-string|"'%(levelname)s %(name)s [%(request_id)s '"
-nl|'\n'
-string|"'%(user_identity)s] %(instance)s'"
-nl|'\n'
-string|"'%(message)s'"
-op|')'
 newline|'\n'
 nl|'\n'
 nl|'\n'
@@ -201,18 +147,29 @@ newline|'\n'
 indent|'    '
 name|'log'
 op|'.'
-name|'set_defaults'
-op|'('
-name|'_DEFAULT_LOGGING_CONTEXT_FORMAT'
-op|','
-name|'_DEFAULT_LOG_LEVELS'
-op|')'
-newline|'\n'
-name|'log'
-op|'.'
 name|'register_options'
 op|'('
 name|'CONF'
+op|')'
+newline|'\n'
+comment|'# We use the oslo.log default log levels which includes suds=INFO'
+nl|'\n'
+comment|'# and add only the extra levels that Nova needs'
+nl|'\n'
+name|'log'
+op|'.'
+name|'set_defaults'
+op|'('
+name|'default_log_levels'
+op|'='
+name|'log'
+op|'.'
+name|'get_default_log_levels'
+op|'('
+op|')'
+op|'+'
+nl|'\n'
+name|'_EXTRA_DEFAULT_LOG_LEVELS'
 op|')'
 newline|'\n'
 name|'options'
