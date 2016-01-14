bb6c993d1fc63a7288081d5d4be347984e369d6c
Stephen Finucane <stephen.finucane@intel.com>
2016-01-14 10:38:33 +0000

    Improve 'virt.driver' conf options documentation
    Change-Id: I1f22235796af0342d35e8d2db603f91922a92293
    Implements: bp centralize-config-options

--- nova/conf/virt.py
+++ nova/conf/virt.py
@@ -45,7 +45,7 @@ op|','
 nl|'\n'
 name|'help'
 op|'='
-string|'"""Defines which physical CPUs (pCPUs) can be used by instance\nvirtual CPUs (vCPUs).\n\nPossible values:\n\n* A comma-separated list of physical CPU numbers that virtual CPUs can be\n  allocated to by default. Each element should be either a single CPU number,\n  a range of CPU numbers, or a caret followed by a CPU number to be\n  excluded from a previous range. For example:\n\n    vcpu_pin_set = "4-12,^8,15"\n\nServices which consume this:\n\n* nova-scheduler\n* nova-compute\n\nRelated options:\n\n* None"""'
+string|'"""Defines which physical CPUs (pCPUs) can be used by instance\nvirtual CPUs (vCPUs).\n\nPossible values:\n\n* A comma-separated list of physical CPU numbers that virtual CPUs can be\n  allocated to by default. Each element should be either a single CPU number,\n  a range of CPU numbers, or a caret followed by a CPU number to be\n  excluded from a previous range. For example:\n\n    vcpu_pin_set = "4-12,^8,15"\n\nServices which consume this:\n\n* ``nova-scheduler``\n* ``nova-compute``\n\nInterdependencies to other options:\n\n* None\n"""'
 op|')'
 newline|'\n'
 nl|'\n'
@@ -60,16 +60,9 @@ nl|'\n'
 string|"'compute_driver'"
 op|','
 nl|'\n'
-DECL|variable|help
 name|'help'
 op|'='
-string|"'Driver to use for controlling virtualization. Options '"
-nl|'\n'
-string|"'include: libvirt.LibvirtDriver, xenapi.XenAPIDriver, '"
-nl|'\n'
-string|"'fake.FakeDriver, ironic.IronicDriver, '"
-nl|'\n'
-string|"'vmwareapi.VMwareVCDriver, hyperv.HyperVDriver'"
+string|'"""Defines which driver to use for controlling virtualization.\n\nPossible values:\n\n* ``libvirt.LibvirtDriver``\n* ``xenapi.XenAPIDriver``\n* ``fake.FakeDriver``\n* ``ironic.IronicDriver``\n* ``vmwareapi.VMwareVCDriver``\n* ``hyperv.HyperVDriver``\n\nServices which consume this:\n\n* ``nova-compute``\n\nInterdependencies to other options:\n\n* None\n"""'
 op|')'
 newline|'\n'
 nl|'\n'
@@ -84,12 +77,9 @@ nl|'\n'
 string|"'default_ephemeral_format'"
 op|','
 nl|'\n'
-DECL|variable|help
 name|'help'
 op|'='
-string|"'The default format an ephemeral_volume will be '"
-nl|'\n'
-string|"'formatted with on creation.'"
+string|'"""The default format an ephemeral_volume will be formatted\nwith on creation.\n\nPossible values:\n\n* ``ext2``\n* ``ext3``\n* ``ext4``\n* ``xfs``\n* ``ntfs`` (only for Windows guests)\n\nServices which consume this:\n\n* ``nova-compute``\n\nInterdependencies to other options:\n\n* None\n"""'
 op|')'
 newline|'\n'
 nl|'\n'
@@ -120,14 +110,9 @@ string|"'space'"
 op|')'
 op|','
 nl|'\n'
-DECL|variable|help
 name|'help'
 op|'='
-string|"'VM image preallocation mode: '"
-nl|'\n'
-string|'\'"none" => no storage provisioning is done up front, \''
-nl|'\n'
-string|'\'"space" => storage is fully allocated at instance start\''
+string|'"""The image preallocation mode to use. Image preallocation allows\nstorage for instance images to be allocated up front when the instance is\ninitially provisioned. This ensures immediate feedback is given if enough\nspace isn\'t available. In addition, it should significantly improve\nperformance on writes to new blocks and may even improve I/O performance to\nprewritten blocks due to reduced fragmentation.\n\nPossible values:\n\n* "none"  => no storage provisioning is done up front\n* "space" => storage is fully allocated at instance start\n\nServices which consume this:\n\n* ``nova-compute``\n\nInterdependencies to other options:\n\n* None\n"""'
 op|')'
 newline|'\n'
 nl|'\n'
@@ -148,10 +133,9 @@ op|'='
 name|'True'
 op|','
 nl|'\n'
-DECL|variable|help
 name|'help'
 op|'='
-string|"'Whether to use cow images'"
+string|'"""Enable use of copy-on-write (cow) images.\n\nQEMU/KVM allow the use of qcow2 as backing files. By disabling this,\nbacking files will not be used.\n\nPossible values:\n\n* True: Enable use of cow images\n* False: Disable use of cow images\n\nServices which consume this:\n\n* ``nova-compute``\n\nInterdependencies to other options:\n\n* None\n"""'
 op|')'
 newline|'\n'
 nl|'\n'
@@ -172,10 +156,9 @@ op|'='
 name|'True'
 op|','
 nl|'\n'
-DECL|variable|help
 name|'help'
 op|'='
-string|"'Fail instance boot if vif plugging fails'"
+string|'"""Determine if instance should boot or fail on VIF plugging timeout.\n\nNova sends a port update to Neutron after an instance has been scheduled,\nproviding Neutron with the necessary information to finish setup of the port.\nOnce completed, Neutron notifies Nova that it has finished setting up the\nport, at which point Nova resumes the boot of the instance since network\nconnectivity is now supposed to be present. A timeout will occur if the reply\nis not received after a given interval.\n\nThis option determines what Nova does when the VIF plugging timeout event\nhappens. When enabled, the instance will error out. When disabled, the\ninstance will continue to boot on the assumption that the port is ready.\n\nPossible values:\n\n* True: Instances should fail after VIF plugging timeout\n* False: Instances should continue booting after VIF plugging timeout\n\nServices which consume this:\n\n* ``nova-compute``\n\nInterdependencies to other options:\n\n* None\n"""'
 op|')'
 newline|'\n'
 nl|'\n'
@@ -196,18 +179,9 @@ op|'='
 number|'300'
 op|','
 nl|'\n'
-DECL|variable|help
 name|'help'
 op|'='
-string|"'Number of seconds to wait for neutron vif plugging '"
-nl|'\n'
-string|"'events to arrive before continuing or failing (see '"
-nl|'\n'
-string|"'vif_plugging_is_fatal). If this is set to zero and '"
-nl|'\n'
-string|"'vif_plugging_is_fatal is False, events should not '"
-nl|'\n'
-string|"'be expected to arrive at all.'"
+string|'"""Timeout for Neutron VIF plugging event message arrival.\n\nNumber of seconds to wait for Neutron vif plugging events to\narrive before continuing or failing (see \'vif_plugging_is_fatal\'). If this is\nset to zero and \'vif_plugging_is_fatal\' is False, events should not be\nexpected to arrive at all.\n\nPossible values:\n\n* A time interval in seconds\n\nServices which consume this:\n\n* ``nova-compute``\n\nInterdependencies to other options:\n\n* None\n"""'
 op|')'
 newline|'\n'
 nl|'\n'
