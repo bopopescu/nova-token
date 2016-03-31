b5b883fc702c85da9f00c8e5d9e0105db2b60755
Paul Murray <pmurray@hpe.com>
2016-03-31 11:39:33 +0100

    Remove extensible resource tracking
    The extensible resource tracker plugins were deprecated in
    Ie4b6670a828d99aebfd348bd700f9f58b3e39fdb - it is now time
    to remove the feature. Providers will no longer be able
    to extend the resource types they want to track.
    
    The compute node stats field is now updated every time
    stats change (as it originally was before ERT). Some tests
    have had stats added to the expected field in assertions
    to account for this information.
    
    Change-Id: Ia34ca9160ca34bcfa3ed56395e1b68ca802c0c51

--- nova/conf/compute.py
+++ nova/conf/compute.py
@@ -343,48 +343,6 @@ name|'True'
 op|')'
 op|','
 nl|'\n'
-name|'cfg'
-op|'.'
-name|'ListOpt'
-op|'('
-string|"'compute_resources'"
-op|','
-nl|'\n'
-DECL|variable|default
-name|'default'
-op|'='
-op|'['
-op|']'
-op|','
-nl|'\n'
-DECL|variable|help
-name|'help'
-op|'='
-string|"'DEPRECATED:The names of the extra resources to track. '"
-nl|'\n'
-string|"'The Extensible Resource Tracker is deprecated and will '"
-nl|'\n'
-string|"'be removed in the 14.0.0 release. If you '"
-nl|'\n'
-string|"'use this functionality and have custom resources that '"
-nl|'\n'
-string|"'are managed by the Extensible Resource Tracker, please '"
-nl|'\n'
-string|"'contact the Nova development team by posting to the '"
-nl|'\n'
-string|"'openstack-dev mailing list. There is no future planned '"
-nl|'\n'
-string|"'support for the tracking of custom resources.'"
-op|','
-nl|'\n'
-DECL|variable|deprecated_for_removal
-name|'deprecated_for_removal'
-op|'='
-name|'True'
-op|')'
-op|','
-nl|'\n'
-nl|'\n'
 op|']'
 newline|'\n'
 nl|'\n'
