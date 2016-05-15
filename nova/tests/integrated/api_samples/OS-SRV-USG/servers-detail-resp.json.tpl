ramielrowe/nova

Launch_at and terminated_at on server(s) response
Implements bp usage-details-on-instance

Change-Id: I0a9101c43a51d597a1eaff9d5a5d08d602024e72

nova/tests/integrated/api_samples/OS-INST-USG/servers-detail-resp.json.tpl
@@ -1,0 +1,57 @@
+{
+    "servers": [
+    {
+            "status": "ACTIVE",
+            "updated": "%(timestamp)s",
+            "OS-INST-USG:launched_at": "%(timestamp)s",
+            "user_id": "fake",
+            "addresses": {
+                "private": [
+                    {
+                        "addr": "%(ip)s",
+                        "version": 4
+                    }
+                ]
+            },
+            "links": [
+                {
+                    "href": "%(host)s/v2/openstack/servers/%(uuid)s",
+                    "rel": "self"
+                },
+                {
+                    "href": "%(host)s/openstack/servers/%(uuid)s",
+                    "rel": "bookmark"
+                }
+            ],
+            "created": "%(timestamp)s",
+            "name": "new-server-test",
+            "image": {
+                "id": "%(uuid)s",
+                "links": [
+                    {
+                        "href": "%(host)s/openstack/images/%(uuid)s",
+                        "rel": "bookmark"
+                    }
+                ]
+            },
+            "id": "%(uuid)s",
+            "accessIPv4": "",
+            "accessIPv6": "",
+            "OS-INST-USG:terminated_at": null,
+            "tenant_id": "openstack",
+            "progress": 0,
+            "flavor": {
+                "id": "1",
+                "links": [
+                    {
+                        "href": "%(host)s/openstack/flavors/1",
+                        "rel": "bookmark"
+                    }
+                ]
+            },
+            "hostId": "%(hostid)s",
+            "metadata": {
+                "My Server Name": "Apache1"
+            }
+    }]
+}

nova/tests/integrated/api_samples/OS-INST-USG/servers-detail-resp.xml.tpl
@@ -1,0 +1,21 @@
+<?xml version='1.0' encoding='UTF-8'?>
+<servers xmlns:OS-INST-USG="http://docs.openstack.org/compute/ext/instance_usage/api/v1.1" xmlns:atom="http://www.w3.org/2005/Atom" xmlns="http://docs.openstack.org/compute/api/v1.1">
+  <server xmlns:OS-INST-USG="http://docs.openstack.org/compute/ext/instance_usage/api/v1.1" xmlns:atom="http://www.w3.org/2005/Atom" xmlns="http://docs.openstack.org/compute/api/v1.1" status="ACTIVE" updated="%(timestamp)s" hostId="%(hostid)s" name="new-server-test" created="%(timestamp)s" userId="fake" tenantId="openstack" accessIPv4="" accessIPv6="" progress="0" id="%(id)s" OS-INST-USG:launched_at="%(timestamp)s" OS-INST-USG:terminated_at="None" >
+    <image id="%(uuid)s">
+      <atom:link href="%(host)s/openstack/images/%(uuid)s" rel="bookmark"/>
+    </image>
+    <flavor id="1">
+      <atom:link href="%(host)s/openstack/flavors/1" rel="bookmark"/>
+    </flavor>
+    <metadata>
+      <meta key="My Server Name">Apache1</meta>
+    </metadata>
+    <addresses>
+      <network id="private">
+        <ip version="4" addr="%(ip)s"/>
+      </network>
+    </addresses>
+    <atom:link href="%(host)s/v2/openstack/servers/%(uuid)s" rel="self"/>
+    <atom:link href="%(host)s/openstack/servers/%(uuid)s" rel="bookmark"/>
+  </server>
+</servers>

nova/tests/integrated/api_samples/all_extensions/extensions-get-resp.json.tpl
@@ -89,6 +89,14 @@
             "updated": "%(timestamp)s"
         },
         {
+            "alias": "OS-INST-USG",
+            "description": "%(text)s",
+            "links": [],
+            "name": "InstanceUsage",
+            "namespace": "http://docs.openstack.org/compute/ext/instance_usage/api/v1.1",
+            "updated": "%(timestamp)s"
+        },
+        {
             "alias": "OS-SCH-HNT",
             "description": "%(text)s",
             "links": [],

nova/tests/integrated/api_samples/all_extensions/extensions-get-resp.xml.tpl
@@ -33,6 +33,9 @@
   <extension alias="OS-FLV-EXT-DATA" updated="%(timestamp)s" namespace="http://docs.openstack.org/compute/ext/flavor_extra_data/api/v1.1" name="FlavorExtraData">
     <description>%(text)s</description>
   </extension>
+  <extension alias="OS-INST-USG" updated="%(timestamp)s" namespace="http://docs.openstack.org/compute/ext/instance_usage/api/v1.1" name="InstanceUsage">
+    <description>%(text)s</description>
+  </extension>
   <extension alias="OS-SCH-HNT" updated="%(timestamp)s" namespace="http://docs.openstack.org/compute/ext/scheduler-hints/api/v2" name="SchedulerHints">
     <description>%(text)s</description>
   </extension>

bopopescu/nova-token

nova/tests/integrated/api_samples/OS-SRV-USG/servers-detail-resp.json.tpl
@@ -1,0 +1,57 @@
+{
+    "servers": [
+    {
+            "status": "ACTIVE",
+            "updated": "%(timestamp)s",
+            "OS-SRV-USG:launched_at": "%(timestamp)s",
+            "user_id": "fake",
+            "addresses": {
+                "private": [
+                    {
+                        "addr": "%(ip)s",
+                        "version": 4
+                    }
+                ]
+            },
+            "links": [
+                {
+                    "href": "%(host)s/v2/openstack/servers/%(uuid)s",
+                    "rel": "self"
+                },
+                {
+                    "href": "%(host)s/openstack/servers/%(uuid)s",
+                    "rel": "bookmark"
+                }
+            ],
+            "created": "%(timestamp)s",
+            "name": "new-server-test",
+            "image": {
+                "id": "%(uuid)s",
+                "links": [
+                    {
+                        "href": "%(host)s/openstack/images/%(uuid)s",
+                        "rel": "bookmark"
+                    }
+                ]
+            },
+            "id": "%(uuid)s",
+            "accessIPv4": "",
+            "accessIPv6": "",
+            "OS-SRV-USG:terminated_at": null,
+            "tenant_id": "openstack",
+            "progress": 0,
+            "flavor": {
+                "id": "1",
+                "links": [
+                    {
+                        "href": "%(host)s/openstack/flavors/1",
+                        "rel": "bookmark"
+                    }
+                ]
+            },
+            "hostId": "%(hostid)s",
+            "metadata": {
+                "My Server Name": "Apache1"
+            }
+    }]
+}

nova/tests/integrated/api_samples/OS-SRV-USG/servers-detail-resp.xml.tpl
@@ -1,0 +1,21 @@
+<?xml version='1.0' encoding='UTF-8'?>
+<servers xmlns:OS-SRV-USG="http://docs.openstack.org/compute/ext/server_usage/api/v1.1" xmlns:atom="http://www.w3.org/2005/Atom" xmlns="http://docs.openstack.org/compute/api/v1.1">
+  <server xmlns:OS-SRV-USG="http://docs.openstack.org/compute/ext/server_usage/api/v1.1" xmlns:atom="http://www.w3.org/2005/Atom" xmlns="http://docs.openstack.org/compute/api/v1.1" status="ACTIVE" updated="%(timestamp)s" hostId="%(hostid)s" name="new-server-test" created="%(timestamp)s" userId="fake" tenantId="openstack" accessIPv4="" accessIPv6="" progress="0" id="%(id)s" OS-SRV-USG:launched_at="%(timestamp)s" OS-SRV-USG:terminated_at="None" >
+    <image id="%(uuid)s">
+      <atom:link href="%(host)s/openstack/images/%(uuid)s" rel="bookmark"/>
+    </image>
+    <flavor id="1">
+      <atom:link href="%(host)s/openstack/flavors/1" rel="bookmark"/>
+    </flavor>
+    <metadata>
+      <meta key="My Server Name">Apache1</meta>
+    </metadata>
+    <addresses>
+      <network id="private">
+        <ip version="4" addr="%(ip)s"/>
+      </network>
+    </addresses>
+    <atom:link href="%(host)s/v2/openstack/servers/%(uuid)s" rel="self"/>
+    <atom:link href="%(host)s/openstack/servers/%(uuid)s" rel="bookmark"/>
+  </server>
+</servers>

nova/tests/integrated/api_samples/all_extensions/extensions-get-resp.json.tpl
@@ -89,6 +89,14 @@
             "updated": "%(timestamp)s"
         },
         {
+            "alias": "OS-SRV-USG",
+            "description": "%(text)s",
+            "links": [],
+            "name": "ServerUsage",
+            "namespace": "http://docs.openstack.org/compute/ext/server_usage/api/v1.1",
+            "updated": "%(timestamp)s"
+        },
+        {
             "alias": "OS-SCH-HNT",
             "description": "%(text)s",
             "links": [],

nova/tests/integrated/api_samples/all_extensions/extensions-get-resp.xml.tpl
@@ -33,6 +33,9 @@
   <extension alias="OS-FLV-EXT-DATA" updated="%(timestamp)s" namespace="http://docs.openstack.org/compute/ext/flavor_extra_data/api/v1.1" name="FlavorExtraData">
     <description>%(text)s</description>
   </extension>
+  <extension alias="OS-SRV-USG" updated="%(timestamp)s" namespace="http://docs.openstack.org/compute/ext/server_usage/api/v1.1" name="ServerUsage">
+    <description>%(text)s</description>
+  </extension>
   <extension alias="OS-SCH-HNT" updated="%(timestamp)s" namespace="http://docs.openstack.org/compute/ext/scheduler-hints/api/v2" name="SchedulerHints">
     <description>%(text)s</description>
   </extension>