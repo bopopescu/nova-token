soulxu/nova

Add v3 api samples for shelve
Adds api sample tests and api samples for shelve

Partially implements blueprint v3-api-unittests
Partially implements blueprint v3-api-specification

Change-Id: Ib485ab042a340c4672ad01fb35d065fd52889b07

nova/tests/integrated/v3/api_samples/os-shelve/server-post-resp.json.tpl
@@ -1,0 +1,16 @@
+{
+    "server": {
+        "admin_pass": "%(password)s",
+        "id": "%(id)s",
+        "links": [
+            {
+                "href": "http://openstack.example.com/v3/servers/%(uuid)s",
+                "rel": "self"
+            },
+            {
+                "href": "http://openstack.example.com/servers/%(uuid)s",
+                "rel": "bookmark"
+            }
+        ]
+    }
+}

nova/tests/integrated/v3/api_samples/os-shelve/server-post-resp.xml.tpl
@@ -1,0 +1,6 @@
+<?xml version='1.0' encoding='UTF-8'?>
+<server xmlns:atom="http://www.w3.org/2005/Atom" xmlns="http://docs.openstack.org/compute/api/v1.1" id="%(id)s" admin_pass="%(password)s">
+  <metadata/>
+  <atom:link href="%(host)s/v3/servers/%(uuid)s" rel="self"/>
+  <atom:link href="%(host)s/servers/%(uuid)s" rel="bookmark"/>
+</server>

bopopescu/nova-token

nova/tests/integrated/v3/api_samples/os-shelve/server-post-resp.json.tpl
@@ -1,0 +1,16 @@
+{
+    "server": {
+        "admin_password": "%(password)s",
+        "id": "%(id)s",
+        "links": [
+            {
+                "href": "http://openstack.example.com/v3/servers/%(uuid)s",
+                "rel": "self"
+            },
+            {
+                "href": "http://openstack.example.com/servers/%(uuid)s",
+                "rel": "bookmark"
+            }
+        ]
+    }
+}

nova/tests/integrated/v3/api_samples/os-shelve/server-post-resp.xml.tpl
@@ -1,0 +1,6 @@
+<?xml version='1.0' encoding='UTF-8'?>
+<server xmlns:atom="http://www.w3.org/2005/Atom" xmlns="http://docs.openstack.org/compute/api/v1.1" id="%(id)s" admin_password="%(password)s">
+  <metadata/>
+  <atom:link href="%(host)s/v3/servers/%(uuid)s" rel="self"/>
+  <atom:link href="%(host)s/servers/%(uuid)s" rel="bookmark"/>
+</server>